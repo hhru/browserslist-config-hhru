5a120a2c3c57e5002226c1a848e73ebdffee7e89
Nikita Bezdolniy <gooverdian@gmail.com>
2021-12-03 14:19:59 +0300

    HH-141388 Include Opera

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = [
     'last 10 Chrome versions',
     'last 10 Firefox versions',
     'Firefox ESR',
-    'last 2 Opera versions',
+    'last 10 Opera versions',
     'last 3 Safari versions', // safari нумерует не мажорные версии. Например 9, 9.1 это 2 разные версии
     'iOS >= 10', // ios нумерует не мажорные версии, а отрезки. Например 9-9.2 это одна версия, а 9.3 другая
     'Explorer >= 11',
