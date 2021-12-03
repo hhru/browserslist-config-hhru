60c870800134ff2ab959c3b6dbd4b3715830e73d
Nikita Bezdolniy <gooverdian@gmail.com>
2021-12-03 14:41:05 +0300

    HH-141388 Modify other versions, add test

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 module.exports = [
     'last 10 Chrome versions',
     'last 10 Firefox versions',
-    'Firefox ESR',
     'last 10 Opera versions',
-    'last 3 Safari versions', // safari нумерует не мажорные версии. Например 9, 9.1 это 2 разные версии
-    'iOS >= 10', // ios нумерует не мажорные версии, а отрезки. Например 9-9.2 это одна версия, а 9.3 другая
-    'Explorer >= 11',
+    'last 10 Safari versions', // safari нумерует не мажорные версии. Например 9, 9.1 это 2 разные версии
+    'last 10 iOS versions', // ios нумерует не мажорные версии, а отрезки. Например 9-9.2 это одна версия, а 9.3 другая
     'last 10 ChromeAndroid versions',
-    'Android >= 4.4',
     'last 10 Edge versions',
-    'last 1 ie_mob versions',
+    'Firefox ESR',
+    'Explorer >= 11',
+    'Android >= 5',
 ];
