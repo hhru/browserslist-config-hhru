a513605253b5219bc729574f209afd57a3a57609
i.khokhryakov <ilyahohryakov@gmail.com>
2022-06-23 18:07:57 +0300

    HH-154205 fix MIUI rule

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = [
-    'last 10 Chrome versions',
+    'Chrome >= 79', // Mi Browser (MIUI), который мы поддерживаем, browserslist определяет как Chrome. Версия в конфиге должна быть не меньше версии Chrome, используемой в предпоследнем мажоре MIUI.
     'last 10 Firefox versions',
     'last 10 Opera versions',
     'last 10 Safari versions', // safari нумерует не мажорные версии. Например 9, 9.1 это 2 разные версии
     'last 10 iOS versions', // ios нумерует не мажорные версии, а отрезки. Например 9-9.2 это одна версия, а 9.3 другая
-    'ChromeAndroid >= 79', // Mi Browser (MIUI), который мы поддерживаем, browserslist определяет как мобильный Chrome. Версия в конфиге должна быть не меньше версии Chrome, используемой в предпоследнем мажоре MIUI.
+    'ChromeAndroid', // всегда возвращает последнюю версию, сравнение версий работает только для десктопного Chrome
     'last 10 Edge versions',
     'Firefox ESR',
     'Android >= 5',
