b5a7cad352ac3659ecbd5fdea50988c96020b934
i.khokhryakov <ilyahohryakov@gmail.com>
2022-06-23 17:07:21 +0300

    HH-154194 add ChromeAndroid rule because of MIUI

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = [
     'last 10 Opera versions',
     'last 10 Safari versions', // safari нумерует не мажорные версии. Например 9, 9.1 это 2 разные версии
     'last 10 iOS versions', // ios нумерует не мажорные версии, а отрезки. Например 9-9.2 это одна версия, а 9.3 другая
-    'last 10 ChromeAndroid versions',
+    'ChromeAndroid >= 79', // Mi Browser (MIUI), который мы поддерживаем, browserslist определяет как мобильный Chrome. Версия в конфиге должна быть не меньше версии Chrome, используемой в предпоследнем мажоре MIUI.
     'last 10 Edge versions',
     'Firefox ESR',
     'Android >= 5',
